3ad7c081c4c28da2735d2731df94a3f1361be4d2
Zekroth <edoardo.barchiesi@hotmail.it>
2020-01-16 12:06:13 +0100

    Update classe ConsoleStart

--- Examples/src/com/examples/Employee.java
+++ Examples/src/com/examples/Employee.java
@@ -1,12 +1,17 @@
 package com.examples;
 
 public class Employee {
-	String nome;
-	String cognome;
-	int anni;
-	String sesso;
-	int stipendio;
-	String matricola;
+	public String nome;
+	public String cognome;
+	public int anni;
+	public String sesso;
+	public int stipendio;
+	public String matricola;
+	public int gglavorati;
+	
+	private String errors = "";
+	private boolean isValid = true;
+	
 	/**
 	 * @param nome
 	 * @param cognome
@@ -14,8 +19,10 @@ public class Employee {
 	 * @param sesso
 	 * @param stipendio
 	 * @param matricola
+	 * @param gglavorati 
+	 * @throws Exception 
 	 */
-	public Employee(String nome, String cognome, int anni, String sesso, int stipendio, String matricola) {
+	public Employee(String nome, String cognome, int anni, String sesso, int stipendio, String matricola, int gglavorati) throws Exception {
 		super();
 		this.nome = nome;
 		this.cognome = cognome;
@@ -23,7 +30,25 @@ public class Employee {
 		this.sesso = sesso;
 		this.stipendio = stipendio;
 		this.matricola = matricola;
-
+		this.gglavorati = gglavorati;
+		
+		if(!Validate()) {
+			throw new Exception(errors);
+		}
+		
+	}
+	public boolean Validate() {
+		if(anni > 100) {
+			errors += "L'età non può essere superiore a 100 anni;";
+		}
+		if(anni < 18) {
+			errors += "L'età non può essere inferiore a 18 anni;";
+		}
+		//Aggiungere controlli vari
+		if(errors != "") {
+			isValid = false;
+		}
+		return isValid;
 	}
 	
 	
