6f3a6f62d2d12275974282690d454fe6c7631a9f
Edoardo barchiesi <edoardo.barchiesi@hotmail.it>
2020-02-05 15:29:54 +0100

    Nella classe corso ho messo 2 variabili private di tipo stringa titolo e codice, successivamente ho creato un costruttore corso con parametri

---
+++ GestoreScolastico/src/Models/Corso.java
@@ -0,0 +1,40 @@
+package Models;
+
+import java.util.List;
+
+public class Corso {
+	
+	private String titolo;
+	private String codice;
+	protected List<Studente> iscrizioni;
+	protected List<Professore> professori;
+	
+	public Corso(String titolo , String codice) {
+		this.titolo = titolo;	
+		this.codice = codice;
+	}
+	
+	public String getTitolo() {
+		return titolo;
+	}
+	
+	protected void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+	
+	public String getCodice() {
+		return codice;
+	}
+	
+	protected void setCodice(String codice) {
+		this.codice = codice;
+	}
+	
+	public void stampaIscritti() {
+		
+	}
+	
+
+	
+
+}
