2177f2afd3b7de8def3035ae53b096993f26531f
Zekroth <edoardo.barchiesi@hotmail.it>
2020-02-13 15:59:45 +0100

    Excercises with String.format()

---
+++ Examples/src/com/interfaces/console/StringFormatTeest.java
@@ -0,0 +1,11 @@
+package com.interfaces.console;
+
+public class StringFormatTeest {
+
+	public static void main(String[] args) {
+		String sf1 = String .format("hi ", args[0]);
+		System.out.println(sf1);
+		System.out.println(args[0]);
+	}
+
+}
